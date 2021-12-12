b1712e2125d79d79544f886a9adcec1bb4188565
FrontEnd07 <yunusov9433@gmail.com>
2021-12-12 23:58:27 +0500

    add code

--- src/view/Vendor/Main/Success/Success.jsx
+++ src/view/Vendor/Main/Success/Success.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import style from "./Success.module.scss";
-
+import { Button } from "@components/Buttons"
+import { Link } from "react-router-dom";
 const Success = () => {
     return (
         <div className={style.main}>
-            Success
+            <div className={style.success}>
+
+            </div>
+            <div className={style.button}>
+                <Button>
+                    <Link to="/">
+                        ГЛАВНАЯ СТРАНИЦА
+                    </Link>
+                </Button>
+            </div>
         </div>
     );
 };
