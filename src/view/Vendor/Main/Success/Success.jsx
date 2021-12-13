0db5a52c8b66ca37b1293766e4365368886582da
FrontEnd07 <yunusov9433@gmail.com>
2021-12-13 17:21:30 +0500

    update

--- src/view/Vendor/Main/Success/Success.jsx
+++ src/view/Vendor/Main/Success/Success.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import style from "./Success.module.scss";
 import { Button } from "@components/Buttons"
 import { Link } from "react-router-dom";
+import image from "@assets/Vendor/Success/success.svg";
+import image2 from "@assets/Vendor/Success/successBottom.svg";
+
 const Success = () => {
     return (
         <div className={style.main}>
             <div className={style.success}>
-
+                <div className={style.image}>
+                    <img src={image} alt="" />
+                </div>
+                <div className={style.user}>
+                    <span className={style.name}>
+                        АБДУЛЛО АБДУРАХИМОВ
+                    </span>
+                    <span>
+                        Спасибо за ваш заказ!
+                    </span>
+                    <span>
+                        Наш менеджер свяжется с вами!
+                    </span>
+                </div>
+                <div className={style.image}>
+                    <img src={image2} alt="" />
+                </div>
             </div>
             <div className={style.button}>
                 <Button>
--- src/view/Vendor/Vendor.jsx
+++ src/view/Vendor/Vendor.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import style from "./Vendor.module.scss";
 import { Route, Switch } from "react-router-dom";
 import Header from "@components/Vendor/Header";
-import { Product, Order, Contact } from "@view/Vendor/Main/index"
+import { Product, Order, Contact, Success } from "@view/Vendor/Main/index"
 import Screen from "@view/Vendor/Screen"
 import Main from "@view/Vendor/Main";
 
@@ -27,6 +27,7 @@ const Vendor = () => {
             <Switch>
                 <Route exact path="/" render={() => <Main />} />
                 <Route exact path="/product" render={() => <Product />} />
+                <Route exact path="/success" render={() => <Success />} />
                 <Route exact path="/order" render={() => <Order />} />
                 <Route exact path="/contact" render={() => <Contact />} />
             </Switch>
